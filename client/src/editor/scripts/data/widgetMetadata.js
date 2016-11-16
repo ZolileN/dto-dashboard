2fcc3532ddb4547ea063d7dbd29ec8fe58377801
Elise Chant <elise.chant@gmail.com>
2016-11-17 10:08:18 +1100

    widget metadata pass 2 (#402)
    * add widget help
    
    * add widget help for numeric fields
    
    * update tense
    
    * remove comma from seconds example

--- client/src/editor/scripts/data/widgetMetadata.js
+++ client/src/editor/scripts/data/widgetMetadata.js
@@ -22,14 +22,14 @@ const metadata = {
   'bar': {
     'n': {
       label: '',
-      widget_help: 'This is represented as a single-point bar chart. Enter your data in the fields below.',
+      widget_help: 'This is represented as a bar chart. Enter your data in the fields below.',
       widget_form_help: `These figures are numerical. You can include up to 2 decimal places.`,
       validators: [{validator:'numericNull', message:validators.MESSAGE_NUMERICNULL}]
     },
     's': {
       label: '',
-      widget_help: 'This is represented as a single-point bar chart. Enter your data in the fields below.',
-      widget_form_help: `Enter figures in seconds. For example, 5 hours 2 minutes 35 seconds = 18,155 seconds. It will show on your chart in hours, minutes and seconds.`,
+      widget_help: 'This is represented as a bar chart. Enter your data in the fields below.',
+      widget_form_help: `Enter figures in seconds. For example, 5 hours 2 minutes 35 seconds = 18155 seconds. It will show on your chart in hours, minutes and seconds.`,
       validators: [{validator:'seconds', message:validators.MESSAGE_SECONDS}]
     },
     '%': {
@@ -50,34 +50,34 @@ const metadata = {
   'line': {
     'n': {
       label: '',
-      widget_help: `This is represented as a single-line chart. Enter your data in the fields below.`,
-      widget_form_help: '',
+      widget_help: `This is represented as a line chart. Enter your data in the fields below.`,
+      widget_form_help: `These figures are numerical. You can include up to 2 decimal places.`,
       validators: [{validator:'numericNull', message:validators.MESSAGE_NUMERICNULL}]
     },
     's': {
       label: '',
-      widget_help: `This is represented as a single-line chart. Enter your data in the fields below.`,
-      widget_form_help: `Enter figures in seconds. For example, 5 hours 2 minutes 35 seconds = 18,155 seconds. It will show on your chart in hours, minutes and seconds.`,
+      widget_help: `This is represented as a line chart. Enter your data in the fields below.`,
+      widget_form_help: `Enter figures in seconds. For example, 5 hours 2 minutes 35 seconds = 18155 seconds. It will show on your chart in hours, minutes and seconds.`,
       validators: [{validator:'seconds', message:validators.MESSAGE_SECONDS}]
     },
     '%': {
       label: '',
-      widget_help: `This is represented as a single-line chart. Enter your data in the fields below.`,
-      widget_form_help: '',
+      widget_help: `This is represented as a line chart. Enter your data in the fields below.`,
+      widget_form_help: `These figures are percentages and can’t add up to more than 100. You can include up to 2 decimal places — for example, 32.25.`,
       validators: [{validator:'percentile', message:validators.MESSAGE_PERCENTILE}]
     }
   },
   'sparkline': {
     '%': {
       label: '',
-      widget_help: '',
-      widget_form_help: '',
+      widget_help: ``,
+      widget_form_help: `This figure is a percentage and can't be more than 100. You can include up to 2 decimal places — for example, 32.25.`,
       validators: [{validator:'percentile', message:validators.MESSAGE_PERCENTILE}]
     },
     'n': {
       label: '',
-      widget_help: '',
-      widget_form_help: '',
+      widget_help: ``,
+      widget_form_help: `These figures are numerical. You can include up to 2 decimal places.`,
       validators: [{validator:'numericNull', message:validators.MESSAGE_NUMERICNULL}]
     }
   },
@@ -99,7 +99,7 @@ const metadata = {
     '%': {
       label: '',
       widget_help: `This is represented as a pie chart. Enter your data in the fields below.`,
-      widget_form_help: 'These figures are percentages and can’t add up to more than 100. You can include up to 2 decimal places — for example, 32.25. You can have up to 6 sets of figures.',
+      widget_form_help: 'These figures are percentages and can’t add up to more than 100. You can include up to 2 decimal places — for example, 32.25.',
       validators: [{validator:'percentile', message:validators.MESSAGE_PERCENTILE}]
     }
   }
