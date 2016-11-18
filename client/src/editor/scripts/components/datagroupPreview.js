adbe2ff4dedcbd50d0743fd36cdd0c4aebd1a05c
Elise Chant <elise.chant@gmail.com>
2016-11-18 13:49:59 +1100

    fix - units in Dashboard Widget list previews are missing units (#418)
    * Units in Dashboard Widget list previews are missing units
    
    * humanise

--- client/src/editor/scripts/components/datagroupPreview.js
+++ client/src/editor/scripts/components/datagroupPreview.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import { isEmpty } from 'lodash';
 
 import LegendDot, { getPaletteColor } from './svgs/legendDot';
+import { getHumanisedUnits } from './../helpers/dataset';
 
 
 const Preview = ({recentDatagroupset}) => {
@@ -14,13 +15,17 @@ const Preview = ({recentDatagroupset}) => {
         if (!(group.datapoint && group.datapoint.value)) {  // todo - remove
           console.warn('datapoint is null, this means your widget has no data for this slice of time which is probably an error in the data integrity', group);
         }
+
+        let units = getHumanisedUnits(group.dataset.units);
+        let unitsStr = units ? ` ${units}` : '';
+
         return (
           <div key={idx} className="preview-table">
             <span className="key">
               <LegendDot color={getPaletteColor(idx)} />
             </span>
             <span className="description">{group.dataset.label}</span>
-            <span className="value">{group.datapoint && group.datapoint.value ? group.datapoint.value  : 'No data'}</span>
+            <span className="value">{group.datapoint && `${group.datapoint.value}${unitsStr}` || 'No data'}</span>
             {/*<span className="value">{group.datapoint.value || 'No data'}</span>*/}
           </div>
         );
