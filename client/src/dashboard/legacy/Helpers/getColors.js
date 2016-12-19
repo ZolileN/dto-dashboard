eb0f5fd016f56ec827dc5cea5396e8396d70ca9e
elisechant <elise.chant@gmail.com>
2016-12-19 10:48:44 +0700

    style(chart color palette): add 2 more colors to color palette

--- client/src/dashboard/legacy/Helpers/getColors.js
+++ client/src/dashboard/legacy/Helpers/getColors.js
@@ -1,6 +1,15 @@
 // note: additions to this file should comparitatively update
 // referenced at client/src/dashboard/legacy/Helpers/strokeDashes.js
 function getColors() {
-  return ['#4892c0', '#75a370', '#f2b038', '#47bcac', '#7066a5' ,'#afa545'];
+  return [
+    '#4892c0',  /* blue */
+    '#75a370',  /* dark green */
+    '#f2b038',  /* yellow */
+    '#ffab91',  /* salmon */
+    '#f8bbd0',  /* pink */
+    '#47bcac',  /* turquoise */
+    '#7066a5',  /* purple */
+    '#afa545',  /* light green */
+  ];
 }
 module.exports = getColors;
