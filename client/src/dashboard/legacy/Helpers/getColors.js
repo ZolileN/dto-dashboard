8b376065adc17a4cdbfa7a134684f6dc184c8675
elisechant <elise.chant@gmail.com>
2016-12-22 12:01:14 +1100

    chore(dashboard): changes color palettes

--- client/src/dashboard/legacy/Helpers/getColors.js
+++ client/src/dashboard/legacy/Helpers/getColors.js
@@ -2,14 +2,15 @@
 // referenced at client/src/dashboard/legacy/Helpers/strokeDashes.js
 function getColors() {
   return [
-    '#4892c0',  /* blue */
-    '#75a370',  /* dark green */
-    '#f2b038',  /* yellow */
-    '#ffab91',  /* salmon */
-    '#f8bbd0',  /* pink */
-    '#47bcac',  /* turquoise */
-    '#7066a5',  /* purple */
-    '#afa545',  /* light green */
+    '#4892C0',  /* light blue */
+    '#75A370',  /* dark green */
+    '#F5D900',  /* yellow */
+    '#7066A5',  /* light purple */
+    '#F8BBD0',  /* pink */
+    '#47BCAC',  /* turquoise */
+    '#5345AD',  /* purple */
+    '#AFA545',  /* olive */
+    '#CB6935',  /* orange */
   ];
 }
 module.exports = getColors;
