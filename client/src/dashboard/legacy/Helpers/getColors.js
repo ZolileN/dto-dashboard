5c18637e73f39701c763178ce7c1eefc16618391
Elise Chant <elise.chant@gmail.com>
2017-01-05 14:29:28 +1100

    fix(charts): add comments to explain hard coded unconnected strokes and colors (#506)

--- client/src/dashboard/legacy/Helpers/getColors.js
+++ client/src/dashboard/legacy/Helpers/getColors.js
@@ -1,3 +1,5 @@
+// note: additions to this file should comparitatively update
+// referenced at client/src/dashboard/legacy/Helpers/strokeDashes.js
 function getColors() {
   return ['#4892c0', '#75a370', '#f2b038', '#47bcac', '#7066a5' ,'#afa545'];
 }
