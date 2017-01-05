3cee434b6a961d5cf0e1ac4e25a1c63036ccc82e
elisechant <elise.chant@gmail.com>
2017-01-05 14:23:54 +1100

    fix(charts): add comments to explain hard coded unconnected strokes and colors

--- client/src/dashboard/legacy/Helpers/strokeDashes.js
+++ client/src/dashboard/legacy/Helpers/strokeDashes.js
@@ -8,6 +8,8 @@ const strokeDashes = [
   '16,6',
   '6,2',
 
+  // note: repeat in case they run out, currently we only have 7 colors
+  // referenced at client/src/dashboard/legacy/Helpers/getColors.js
   '2,6',
   '2,2',
   '8,6',
