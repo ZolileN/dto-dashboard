93d60435b9885351a941974b29f53789802bca88
Josh Mostafa <micapam@gmail.com>
2017-01-06 14:18:31 +1100

    Don't assume token in session belongs to current user (#514)

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -14,6 +14,10 @@ class EditorController < ApplicationController
   end
 
   def generate_session_token!
-    session[:session_token] ||= current_user.generate_session_token!
+    if session_token.present?
+      session[:session_token] = session_token
+    else
+      session[:session_token] = current_user.generate_session_token!
+    end
   end
 end
