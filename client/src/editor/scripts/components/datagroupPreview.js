ea4bc27fe734d1791683ba7a0650bbcbe514b120
elisechant <elise.chant@gmail.com>
2017-01-17 16:28:51 +1100

    refactor(preview): make formatValue it's own function so it's ready for test

--- client/src/editor/scripts/components/datagroupPreview.js
+++ client/src/editor/scripts/components/datagroupPreview.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import LegendDot, { getPaletteColor } from './svgs/legendDot';
@@ -6,39 +7,47 @@ import { getHumanisedVeryShortDate } from './../utils/humanisedDates';
 import {formatCurrency2dp} from './../utils/formatCurrency';
 import {formatPercentile2dp} from './../utils/formatPercentile';
 
-const Preview = ({recentDatagroupset}) => {
 
+/**
+ * Format and decorate value provided for the View
+ * @param value {Number|String<Number>}
+ * @param units {String}
+ * @returns {String}
+ */
+const formatValue = (value, units) => {
+  if (typeof value === 'undefined') {
+    console.warn('Group has no datapoint. Something is very wrong.');
+    return null;
+  }
+  let formattedValue;
+  let units = getHumanisedUnits(units);
+  let unitsStr = units ? ` ${units}` : '';
+  if (value === null) {
+    formattedValue = 'No data';
+  } else {
+    if (units === '$') {
+      formattedValue = formatCurrency2dp(value) + unitsStr;
+    } else if (units === '%') {
+      formattedValue = formatPercentile2dp(value) + unitsStr;
+    } else {
+      formattedValue = value + unitsStr;
+    }
+  }
+  return formattedValue;
+};
+
+const Preview = ({recentDatagroupset}) => {
   return (
     <div className="preview">
       <p className="most-recent-text">Most recent data: <span className="strong">{getHumanisedVeryShortDate(recentDatagroupset.recentKey)}</span></p>
-
       {recentDatagroupset.groups.map((group, idx) => {
-        let value;
-        let units = getHumanisedUnits(group.dataset.units);
-        let unitsStr = units ? ` ${units}` : '';
-        if (group.datapoint) {
-          if (group.datapoint.value === null) {
-            value = 'No data';
-          } else {
-            if (units === '$') {
-              value = formatCurrency2dp(group.datapoint.value) + unitsStr;
-            } else if (units === '%') {
-              value = formatPercentile2dp(group.datapoint.value) + unitsStr;
-            } else {
-              value = group.datapoint.value + unitsStr;
-            }
-          }
-        } else {
-          console.warn('Group has no datapoint. Something is very wrong.');
-          return null;
-        }
         return (
           <div key={idx} className="preview-table">
             <span className="key">
               <LegendDot color={getPaletteColor(idx)} />
             </span>
             <span className="description">{group.dataset.label}</span>
-            <span className="value">{value}</span>
+            <span className="value">{formatValue(group.datapoint.value, group.dataset.units)}</span>
           </div>
         );
       })}
